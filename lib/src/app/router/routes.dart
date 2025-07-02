a2a3ca4a4b336db9a7de0710b237b205c1b5d1e8
Jheferson Reis <jhefersonvds@gmail.com>
2025-07-01 22:15:43 -0300

    feat: create app routes

---
+++ lib/src/app/router/routes.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:organik_vendas/src/features/customers/view/customers_form_page.dart';
+import 'package:organik_vendas/src/features/customers/view/customers_page.dart';
+import 'package:organik_vendas/src/features/home/view/home_page.dart';
+import 'package:organik_vendas/src/features/main/view/main_page.dart';
+import 'package:organik_vendas/src/features/products/view/products_page.dart';
+import 'package:organik_vendas/src/features/reports/view/reports_page.dart';
+import 'package:organik_vendas/src/features/sales/view/sales_page.dart';
+
+class Routes {
+  Routes._();
+
+  static final _rootNavigatorKey = GlobalKey<NavigatorState>();
+
+  static final _shellHomeNavigatorKey = GlobalKey<NavigatorState>();
+
+  static final _shellSalesNavigatorKey = GlobalKey<NavigatorState>();
+
+  static final _shellProductsNavigatorKey = GlobalKey<NavigatorState>();
+
+  static final _shellCustomersNavigatorKey = GlobalKey<NavigatorState>();
+
+  static final _shellReportsNavigatorKey = GlobalKey<NavigatorState>();
+
+  static final GoRouter router = GoRouter(
+    initialLocation: '/',
+    navigatorKey: _rootNavigatorKey,
+    debugLogDiagnostics: true,
+    routes: <RouteBase>[
+      StatefulShellRoute.indexedStack(
+        builder: (context, state, navigationShell) {
+          return MainPage(navigationShell: navigationShell);
+        },
+        branches: [
+          StatefulShellBranch(
+            navigatorKey: _shellHomeNavigatorKey,
+            routes: [
+              GoRoute(
+                path: '/',
+                name: "Home",
+                builder: (context, state) {
+                  return const HomePage();
+                },
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            navigatorKey: _shellSalesNavigatorKey,
+            routes: [
+              GoRoute(
+                path: '/sales',
+                name: "Sales",
+                builder: (context, state) {
+                  return const SalesPage();
+                },
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            navigatorKey: _shellProductsNavigatorKey,
+            routes: [
+              GoRoute(
+                path: '/products',
+                name: "Products",
+                builder: (context, state) {
+                  return const ProductsPage();
+                },
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            navigatorKey: _shellCustomersNavigatorKey,
+            routes: [
+              GoRoute(
+                path: '/customers',
+                name: "Customers",
+                builder: (context, state) {
+                  return const CustomersPage();
+                },
+                routes: [
+                  GoRoute(
+                    path: '/form',
+                    name: "CustomerForm",
+                    builder: (context, state) {
+                      final String? customerId =
+                          state.uri.queryParameters['id'];
+                      return CustomersFormPage(id: customerId);
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            navigatorKey: _shellReportsNavigatorKey,
+            routes: [
+              GoRoute(
+                path: '/reports',
+                name: "Reports",
+                builder: (context, state) {
+                  return const ReportsPage();
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    ],
+  );
+}
