5ee4fb56ed87523b11d64880b2d58c6875073224
Jheferson Reis <jhefersonvds@gmail..com>
2025-07-09 17:32:18 -0300

    fix: navigation fix

--- lib/src/features/main/view/main_page.dart
+++ lib/src/features/main/view/main_page.dart
@@ -1,37 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class MainPage extends StatefulWidget {
+class MainPage extends StatelessWidget {
   final StatefulNavigationShell navigationShell;
 
   const MainPage({super.key, required this.navigationShell});
 
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  int _selectedIndex = 0;
-
   void _goBranch(int index) {
-    widget.navigationShell.goBranch(index, initialLocation: index == widget.navigationShell.currentIndex);
+    navigationShell.goBranch(index, initialLocation: index == navigationShell.currentIndex);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: widget.navigationShell,
+      body: navigationShell,
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         selectedItemColor: const Color(0xFF248f3d),
         unselectedItemColor: Colors.grey,
-        currentIndex: _selectedIndex,
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-          _goBranch(_selectedIndex);
-        },
+        currentIndex: navigationShell.currentIndex,
+        onTap: _goBranch,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Início'),
           BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Vendas'),
